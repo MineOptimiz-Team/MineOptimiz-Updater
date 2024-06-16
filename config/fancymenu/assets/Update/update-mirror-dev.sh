5b165015059f21779eb661b293c825006bee149c
SmallMushroom-offical <jxk370@126.com>
2024-06-16 09:55:43 +0800

    edit example

--- config/fancymenu/assets/Update/update-mirror-dev.sh
+++ config/fancymenu/assets/Update/update-mirror-dev.sh
@@ -18,7 +18,7 @@ gitwebsite=examplewebsite
 mirrorwebsite=https://$gitwebsite/$mirrorname/$mirrorowner
 git clone $mirrorwebsite.git
 echo "正在切换到“branch”分支以进行复制"
-cd "mirror"
+cd $mirrorname
 git checkout branch
 cd ..
 echo "正在删除旧版本文件..."
@@ -27,13 +27,13 @@ rm -rf ./CustomSkinLoader
 find ./mods -type f ! -name "exordium*" ! -name "entityculling*" ! -name "fancymenu*" ! -name "notenoughanimations*" ! -name "viafabricplus*" ! -name "voicechat*" ! -name "skinlayers3d*" ! -name "OptiFine*" -exec rm -f {} +
 rm -rf ./resourcepacks
 echo "正在复制新的文件..."
-cp -r "./mirror/overrides/config" ./ || exit 1
-cp -r "./mirror/overrides/CustomSkinLoader" ./ || exit 1
-cp -r "./mirror/overrides/mods" ./ || exit 1
-cp -r "./mirror/overrides/resourcepacks" ./ || exit 1
+cp -r "./$mirrorname/overrides/config" ./ || exit 1
+cp -r "./$mirrorname/overrides/CustomSkinLoader" ./ || exit 1
+cp -r "./$mirrorname/overrides/mods" ./ || exit 1
+cp -r "./$mirrorname/overrides/resourcepacks" ./ || exit 1
 echo "复制完成"
 echo "正在删除临时文件"
-rm -rf "./mirror" || echo "删除失败！下次更新可能会出问题！"
+rm -rf "./$mirrorname" || echo "删除失败！下次更新可能会出问题！"
 echo "更新完成"
 read -p "请按回车键退出" finish
 exit
